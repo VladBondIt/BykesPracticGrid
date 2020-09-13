bca691137a89e7d9b9c4d928acf995abddbb87c1
Vlad Bond <foolowin@gmail.com>
2020-09-13 20:44:07 +0300

    tbc

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const shopBg = document.querySelector('.shop__link');
-const shopTrigger = document.querySelector('.shop__link-h');
+const shopBg = document.getElementById('bg');
+const shopTrigger = document.getElementById('trigger');
 
 shopTrigger.onmouseenter = () => {
     shopBg.style.backgroundColor = 'rgba(100,194,158,0.8)';
