38c94841d38e7f062130b62eea2d857bdf13b0bb
Vlad Bond <foolowin@gmail.com>
2020-09-14 17:54:02 +0300

    tbc

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__left-submenu,.header__right-submenu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+// .header__left-submenu,.header__right-submenu
+// .header__menu
