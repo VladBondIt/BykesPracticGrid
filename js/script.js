271cb3b28b42f92a7d914a6e88a8ae31eb37ac2b
Vlad Bond <foolowin@gmail.com>
2020-10-08 12:17:52 +0300

    fix_hover

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const bgs = document.querySelectorAll('.bg');
 for (let i = 0; i < bgTriggs.length; i++) {
     bgTriggs[i].addEventListener('mousemove', () => {
         for (let i2 = 0; i2 < bgs.length + 1; i2++) {
-            bgs[i2];
             if (i === i2) {
                 bgs[i2].style.backgroundColor = 'rgba(100,194,158,0.6)';
             } else {
